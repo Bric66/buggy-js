017d427e2f920e9c26259982f38cb1e6f4fe0139
bootcode92 <contact@bootcode.co>
2022-10-16 14:08:14 +0200

    adding extra function

--- buggy.js
+++ buggy.js
@@ -1,4 +1,4 @@
-// https://developer.mozilla.org/en-US/docs/web/javascript/reference/statements/export
+// I should be defined and transform in boolean.
 const isWorking = process.env.IS_WORKING;
 
 if (!isWorking || typeof isWorking !== 'boolean') {
@@ -17,23 +17,40 @@ function getStatus() {
 getStatus('it works');
 
 
-// Am i missing something ?
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
-const user = {
-    firstName: 'Benjamin',
-    lastName: 'Castaldi',
+// You should define the missing informations in this object.
+// The missing information is described in the getFusilAssaut function.
+const spartan = {
+    firstName: 'John',
+    lastName: '117',
     age: 56,
-    occupation: 'TV Presentator',
+    occupation: 'Master Chief',
+    company: 'Microsoft',
 }
 
 // Add the missing key in the user object.
-function getUser() {
+function getFusilAssaut() {
     console.log('here the childrens');
-    console.log(user.childrens);
-    return user.childrens[0].firstName;
+    console.log(spartan.weapons);
+    // On fait le -1 parce que en programmation on compte à partir du 0 
+    // mais le .length retourne le total sans commencer par 0.
+    return spartan.weapons[spartan.weapons.length - 1] === "Fusil d'assaut MA5B"; // Should be "Fusil d'assaut MA5B"
 }
 
-getUser();
+getFusilAssaut();
+
+function addWeapon(name) {
+    spartan.weapons.push(name);
+}
+
+// Do i forget to call something ?
+if (!spartan.weapons.includes('sniper')) {
+    throw new Error('John 117 should have a sniper too ....');
+}
+
+
+// Oopsy, we add sniper, now this function does not work anymore...
+// Look how to find for exact value in array in javascript.
+getFusilAssaut(); // Dont remove me.
 
 
 // Make sure to convert string to number when making addition.
